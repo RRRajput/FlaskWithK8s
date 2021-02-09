4914a4dbb6694c68c987bdfe528310b477d27058
Rehan Rajput <kryptdervel@gmail.com>
2021-02-09 19:24:49 +0100

    Added the abstract database adapter

---
+++ DatabaseAdapter.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 19:17:43 2021
+
+@author: Rehan Rajput
+"""
+
+from abc import ABC, abstractmethod
+
+class IDatabaseAdapter(ABC):
+    
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def loadTables(self):
+        pass
+    
+    @abstractmethod
+    def isCustomerPresent(self):
+        pass
+    
+    @abstractmethod
+    def isIPBlacklisted(self):
+        pass
+    
+    @abstractmethod
+    def isUserAgentBlacklisted(self):
+        pass
+    
+    @abstractmethod
+    def insertValidHourlyStat(self):
+        pass
+    
+    @abstractmethod
+    def insertInvalidHourlyStat(self):
+        pass
+    
+    @abstractmethod
+    def generateStatistics(self):
+        pass
+    
+    
